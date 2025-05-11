37e882e6262ceae230daab518416f46ebea3fa33
arnaudlanoizele <10476@holbertonstudents.com>
2025-05-11 15:30:46 +0200

    What is your name

---
+++ python1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# Ask the user to input their name and assign it to a variable named name
+
+name = input("What is your name")
+
+# Print out hello followed the name they entered
+
+print("hello ", name)
